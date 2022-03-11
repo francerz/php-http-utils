af21dd27456427b71c7b786efe0ba9a00805dbf4
Francerz <francerzam@hotmail.com>
2022-03-11 12:16:43 -0600

    FIX siteUrl and baseUrl check

--- src/functions.php
+++ src/functions.php
@@ -4,14 +4,14 @@ namespace Francerz\Http\Utils;
 
 use Francerz\Http\Utils\UriHelper;
 
-if (!function_exists('siteUrl')) {
+if (!function_exists('\Francerz\Http\Utils\siteUrl')) {
     function siteUrl(?string $path = null, array $sapiVars = [])
     {
         return UriHelper::getSiteUrl($path, $sapiVars);
     }
 }
 
-if (!function_exists('baseUrl')) {
+if (!function_exists('\Francerz\Http\Utils\baseUrl')) {
     function baseUrl(?string $path = null, array $sapiVars = [])
     {
         return UriHelper::getBaseUrl($path, $sapiVars);
